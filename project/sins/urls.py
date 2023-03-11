cb194fcc6b68446a77d5b6fca9d90a0ee664c0a1
unknown <henochyanuar13@gmail.com>
2023-03-11 18:58:36 +0700

    Create User CRUD

--- project/sins/urls.py
+++ project/sins/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from . views import admin, kelas, tambahKelas, postDatakelas
+from . views import admin, users, tambahUser, postDatauser, updateDatauser, postUpdateuser, deleteDatauser, postDeleteuser, kelas, tambahKelas, postDatakelas, updateDatakelas
 
 urlpatterns = [
    # path('',index,name='index'),
+   
+   # Url For admin/user page
    path('admin',admin, name="admin"),
+   path('indexUsers',users, name="indexusers"),
+   path('tambahUser', tambahUser, name="tambahuser"),
+   path('postDatauser', postDatauser, name="postdatauser"),
+   path('updateDatauser/<str:id_user>', updateDatauser, name="updatedatauser"),
+   path('postUpdateuser', postUpdateuser, name="postupdatedatauser"),
+   path('deleteDatauser/<str:id_user>', deleteDatauser, name="deletedatauser"),
+   path('postDeleteuser/<str:id_user>', postDeleteuser, name="postdeleteuser"),
+
+
+
    path('indexKelas',kelas, name="indexkelas"),
    path('tambahKelas', tambahKelas, name="tambahkelas"),
    path('postDatakelas',postDatakelas, name="postdatakelas"),
+   path('updateDatakelas',updateDatakelas, name="updatedatakelas"),
 ]
--- project/sins/views.py
+++ project/sins/views.py
@@ -1,11 +1,86 @@
 from django.shortcuts import render, redirect
-from .models import Kelas
+from .models import User, Kelas
 from django.contrib import messages
 
-# Create your views here.
-
+# Create Views For Users CRUD
 def admin(request):
     return render(request,'admin/admin.html')
+
+def users(request):
+    data_user = User.objects.all()
+    context = {
+        'data_user': data_user
+    }
+    return render(request, 'admin/users/index.html', context)
+
+def tambahUser(request):
+    return render(request, 'admin/users/tambahData.html')
+
+def postDatauser(request):
+    id_user = request.POST['id_user']
+    username = request.POST['username']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+    role = request.POST['role']
+
+    if User.objects.filter(id_user=id_user).exists():
+        messages.error(request, 'User already exists')
+    else:
+        if password == password2:
+            tambah_user = User(
+                id_user=id_user,
+                username=username,
+                password=password,
+                role=role,
+            )
+            tambah_user.save()
+            messages.success(request, 'User saved successfully')
+        else:
+            messages.error(request, 'User do not match')
+    return redirect('/indexUsers')
+
+def updateDatauser(request, id_user):
+    data_user = User.objects.get(id_user=id_user)
+    context = {
+        'data_user': data_user
+    }
+    return render(request, 'admin/users/updateData.html', context)
+    
+def postUpdateuser(request):
+    id_user = request.POST['id_user']
+    username = request.POST['username']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+    role = request.POST['role']
+
+    user = User.objects.get(id_user=id_user)
+    if password == password2:
+        user.username = username
+        user.password = password
+        user.role = role
+        user.save()
+        messages.success(request, 'User Update successfully')
+    else:
+        messages.error(request, 'User not do match password')
+    return redirect('/indexUsers')
+
+def deleteDatauser(request, id_user):
+    data_user = User.objects.get(id_user=id_user)
+    context = {
+        'data_user': data_user
+    }
+    return render(request, 'admin/users/deleteData.html', context)
+
+def  postDeleteuser(request, id_user):
+    data_user = User.objects.get(id_user=id_user).delete()
+    messages.success(request, 'User deleted successfully')
+
+    return redirect('/indexUsers')
+
+
+
+
+# Create Views For Kelas CRUD
 
 def kelas(request):
     data_kelas = Kelas.objects.all()
@@ -13,6 +88,7 @@ def kelas(request):
         'data_kelas': data_kelas
     }
     return render(request, 'admin/kelas/index.html', context)
+
 def tambahKelas(request):
     return render(request, 'admin/kelas/tambahData.html')
 
@@ -33,3 +109,9 @@ def postDatakelas(request):
         messages.success(request,'Kelas successfully saved')
     return redirect('/admin/kelas/index')
 
+def updateDatakelas(request, kelas_id):
+    data_kelas = Kelas.objects.all(kelas_id=kelas_id)
+    context = {
+        'data_kelas': data_kelas
+    }
+    return render(request, 'admin/kelas/updateData.html', context)
